60b6f357759c2a6ffb06df13f45ddc14b3648860
cayde-abdo <cayde.abdo@kakaocorp.com>
2019-03-12 23:54:14 +0900

    ch06 end

---
+++ 6.for_of_iterable/child.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-component
+    v-if="field === 'a'"/>
+  <b-component
+    v-else-if="field === 'b'"/>
+  <c-component
+    v-else-if="field === 'c'"/>
+  <d-component
+    v-else-if="field === 'd'"/>
+</template>
+
+<script>
+export default {
+  props: {
+    field: {type: String, default: 'a'}
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
---
+++ 6.for_of_iterable/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <select>
+      <option
+        v-for="option in dropdownList"
+        :key="option.value"
+        @change="changeOption">
+        {{ option.name }}
+      </option>
+    </select>
+    <child-component :field="selectedValue" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dropdownList: [
+      {value: 'a', name: 'a'},
+      {value: 'b', name: 'b'},
+      {value: 'c', name: 'c'},
+      {value: 'd', name: 'd'},
+    ]
+  },
+  data() {
+    return {
+      selectedValue: 'a' // default A
+    }
+  },
+  methods: {
+    changeOption ({ target: { value } }) {
+      this.selectedValue = value
+    }
+  }
+}
+</script>
